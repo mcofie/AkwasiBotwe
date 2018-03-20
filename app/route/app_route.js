64d26224f7c2cb5f7b85530758684f72320d8aa3
Maxwell <geek@max-dex.com>
2018-03-20 03:23:49 +0000

    added mongoose and mLab

--- app/route/app_route.js
+++ app/route/app_route.js
@@ -2,16 +2,47 @@
 //index.js file in the same folder.
 
 module.exports = function(app, db) {
+    //Index Route
+    app.get('/',(req,res)=>{
+        res.send("Hello World");
+    });
+
 
     //Post route to create a given entity
-    app.post('/bot', (req, res) => {
-        // You'll create your note here.
-        let arr = [{'name':'Maxwell Cofie'}]
-        let y = {
-            "name":"Maxwell Cofie",
-            "age":"45"
-        }
-        res.json(y)
+    app.post('api/record', (req, res) => {
+        // const record = {
+        //     type: req.body.type,
+        //     details: req.body.details
+        // };
+        //
+        // db.collection('data').insert(record,(err,result)=>{
+        //     if(err){
+        //         res.send({'error':'An error has occured'});
+        //     } else {
+        //         res.send(result.ops[0])
+        //     }
+        // });
+    });
+
+
+    //Get all records
+    app.get('api/record',(req,res)=>{
+
+    });
+
+    //Query[search] database -> table
+    app.get('api/',()=>{
+
+    });
+
+
+    app.put('api/',()=>{
+
+    });
+
+
+    app.delete('/',()=>{
+
     });
 
 };
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+// const MongoClient    = require('mongodb').MongoClient;
+// const mongoose = require('mongoose');
+// const db = require('./app/config/database');
 const app = express();
 
 //require route file for well structured app route
@@ -9,7 +12,14 @@ require('./app/route')(app, {});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); //URL encoded forms processor
 
+//server port
 let port = 8000;
 
+// MongoClient.connect(db.url, (err, database) => {
+//     if (err) return console.log(err);
 
-app.listen(port,()=>{console.log('Webhook server is listening, port 3000')});
+    // Make sure you add the database name and not the collection name
+    // myDatabase = database.db("bot-api");
+    // require('./app/route')(app,myDatabase);
+    app.listen(port, () => {console.log('We are live on ' + port);});
+// })
